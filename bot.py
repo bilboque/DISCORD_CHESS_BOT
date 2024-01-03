ee3f5436729e445f769edf16aaa874380483c78e
Leo Bonaz <leo-francois.bonaz@etu.unige.ch>
2024-01-03 15:25:56 +0100

    basic ping pong command with options

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-from interactions import Client, Intents, listen, slash_command, SlashContext
+from interactions import Client, Intents, listen
+from interactions import slash_command, slash_option, OptionType
+from interactions import SlashContext, SlashCommandChoice
 
 
 bot = Client(intents=Intents.DEFAULT | Intents.MESSAGE_CONTENT)
@@ -6,24 +8,39 @@ bot = Client(intents=Intents.DEFAULT | Intents.MESSAGE_CONTENT)
 # `DEFAULT` et MESSAGE_CONTENT pour voir les messages
 
 
-# this decorator tells snek that it needs to listen for the corresponding event
-# and run this coroutine
+# Log sur sdtout quand le bot est connecté au server
 @listen()
 async def on_ready():
-    # This event is called when the bot is ready to respond to commands
     print("Ready")
     print(f"This bot is owned by {bot.owner}")
 
 
+# Log sur stdout les messages des users
 @listen()
 async def on_message_create(event):
     if event.message.author != bot.user:
         print(f"message received: {event.message.content}")
 
 
-@slash_command(name="ping", description="ping pong")
-async def ping_pong_command(ctx: SlashContext):
-    await ctx.send("pong")
+# Basic / command avec des optione
+# /ping ping -> pong dans le channel en question,
+# /ping pong -> ping dans le channel en question
+@slash_command(name="ping", description="Usage /ping [ping, pong]")
+@slash_option(
+        name="ping_options",
+        description="ping ou pong",
+        required=True,
+        opt_type=OptionType.STRING,
+        choices=[
+            SlashCommandChoice(name="ping", value="ping"),
+            SlashCommandChoice(name="pong", value="pong"),
+        ]
+)
+async def ping_pong_command(ctx: SlashContext, ping_options):
+    if ping_options == "ping":
+        await ctx.send("pong")
+    elif ping_options == "pong":
+        await ctx.send("ping")
 
 file = open(".token", "r")
 bot.start(file.readline())
